b2906ba1398a03035fc7291c199999bae8921d05
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-06-22 17:31:02 -0300

    formulario de cadastro e login se alternam

---
+++ public/script.js
@@ -0,0 +1,34 @@
+if(document.body.id == "login-cadastro"){
+
+const btnIrParaCadastro = document.getElementById("btnIrParaCadastro")
+const btnIrParaLogin = document.getElementById("btnIrParaLogin")
+
+const cadastroForm = document.getElementById("cadastro-form")
+const loginForm = document.getElementById("login-form")
+
+const btnCadastrar = document.getElementById("btnCadastrar")
+const btnEntar = document.getElementById("btnEntar")
+
+loginForm.style.display = "none";
+
+btnIrParaCadastro.addEventListener("click", () => {
+    cadastroForm.style.display = "flex";
+    loginForm.style.display = "none";
+})
+
+btnIrParaLogin.addEventListener("click", () => {
+    loginForm.style.display = "flex";
+    cadastroForm.style.display = "none";
+})
+
+
+btnCadastrar.addEventListener("click", () => {
+    //fazer requisição de cadastrar usuario
+})
+
+btnEntar.addEventListener("click", () => {
+    //fazer requisição de login
+})
+
+
+}
