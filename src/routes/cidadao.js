e464c240dfc60bea658b8589d3e4425497cfd4de
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-06-23 17:24:48 -0300

    feat: arquivos de routers

---
+++ src/routes/cidadao.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../database/database")
+
+//criar e importar os controllers
+
+
+//exporta o "router"
+module.exports = router
