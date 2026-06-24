273534e2f38a48a58042b5fd9c7b1a84382796aa
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-06-24 16:20:22 -0300

    feat: novas instruções no schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -3,7 +3,11 @@ CREATE DATABASE IF NOT EXISTS jest_db;
 
 use jest_db;
 
+-- drop na tabela associativa primeiro
+DROP TABLE solicitacao;
+
 DROP TABLE usuario;
+DROP TABLE politica;
 
 CREATE TABLE IF NOT EXISTS usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -13,4 +17,53 @@ CREATE TABLE IF NOT EXISTS usuario (
     senha VARCHAR(200)
 );
 
-SELECT * from usuario;
+
+CREATE TABLE IF NOT EXISTS politica (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo varchar(100),
+    descricao varchar(200),
+    publico_alvo varchar(100),
+    local_atuacao varchar(100)
+);
+
+
+CREATE TABLE IF NOT EXISTS solicitacao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario int not null,
+    id_politica int not null,
+    data_solicitacao DATE,
+    data_atualizacao DATE,
+    status_atual ENUM(
+        'em analise', 
+        'aprovado', 
+        'aguardando documento'
+    ) NOT NULL DEFAULT 'aguardando documento',
+
+    FOREIGN KEY (id_usuario) 
+        REFERENCES usuario(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (id_politica) 
+        REFERENCES politica(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+insert into politica (titulo, descricao, publico_alvo, local_atuacao) 
+values (
+    'CAPS - Centro de Atenção Psicossocial',
+    'Atendimento integral em saúde mental com equipe multidisciplinar',
+    'Pessoas com transtornos mentais',
+    'Todo o território nacional'
+);
+
+insert into politica (titulo, descricao, publico_alvo, local_atuacao) 
+values (
+    'Atendimento Psicológico Gratuito',
+    'Sessões de psicoterapia gratuitas em unidades básicas de saúde',
+    'Toda a população que necessite',
+    'Capitais e principais municípios'
+);
+
+SELECT * from politica;
